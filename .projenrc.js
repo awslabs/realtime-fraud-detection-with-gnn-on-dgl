43e4ae7aa07235736dc5389680a489431b4c718a
Magic Chen <haiyunc@amazon.com>
2022-01-15 06:32:21 +0000

    fix: fix ui security

--- .projenrc.js
+++ .projenrc.js
@@ -302,6 +302,8 @@ reactPrj.package.addField('resolutions', {
   'nth-check': '^2.0.1',
   'json-schema': '^0.4.0',
   'node-forge': '^1.2.1',
+  'follow-redirects': '^1.14.7',
+  'shelljs': '^0.8.5',
 });
 
 project.synth();
